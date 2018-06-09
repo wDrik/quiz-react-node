c7d5216438a60de236975cd89292b380435d01fc
Iorgen Wildrik <w.drik@outlook.com>
2018-06-09 20:39:46 -0300

    Configuring Redux architecture

--- quiz-adm/src/index.js
+++ quiz-adm/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // Styles
 // Import Flag Icons Set
@@ -10,17 +11,23 @@ import 'font-awesome/css/font-awesome.min.css';
 // Import Simple Line Icons Set
 import 'simple-line-icons/css/simple-line-icons.css';
 // Import Main styles for this application
-import '../scss/style.scss'
+import '../scss/style.scss';
 // Temp fix for reactstrap
-import '../scss/core/_dropdown-menu-right.scss'
+import '../scss/core/_dropdown-menu-right.scss';
 
 // Containers
-import Full from './containers/Full/'
+import Full from './containers/Full/';
+
+import cfgStore from './store';
+
+const store = cfgStore();
 
 ReactDOM.render((
-  <HashRouter>
-    <Switch>
-      <Route path="/" name="Home" component={Full}/>
-    </Switch>
-  </HashRouter>
+  <Provider store={ store }>
+    <HashRouter>
+      <Switch>
+        <Route path="/" name="Home" component={Full} />
+      </Switch>
+    </HashRouter>
+  </Provider>
 ), document.getElementById('root'));
